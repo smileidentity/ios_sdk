622fac8e53ca32599e4a26bd43616960d5a143bb
Japhet Ndhlovu <jndhlovu54@gmail.com>
2021-06-30 09:05:00 +0200

    version 2.1.0

---
+++ Smile_Identity_SDK.xcframework/ios-arm64/Smile_Identity_SDK.framework/Headers/Smile_Identity_SDK-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "Smile_Identity_SDK.h"
+
+FOUNDATION_EXPORT double Smile_Identity_SDKVersionNumber;
+FOUNDATION_EXPORT const unsigned char Smile_Identity_SDKVersionString[];
+
---
+++ Smile_Identity_SDK.xcframework/ios-arm64/Smile_Identity_SDK.framework/Headers/Smile_Identity_SDK.h
@@ -0,0 +1,19 @@
+//
+//  Smile_Identity_SDK.h
+//  Smile Identity SDK
+//
+//  Created by Janet Brumbaugh on 10/25/18.
+//  Copyright © 2018 Smile Identity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Smile_Identity_SDK.
+FOUNDATION_EXPORT double SmileIDIOSVersionNumber;
+
+//! Project version string for Smile_Identity_SDK.
+FOUNDATION_EXPORT const unsigned char SmileIDIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmileIDIOS/PublicHeader.h>
+
+
